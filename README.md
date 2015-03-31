9e18dd98ce07c4edca4758fff8fcb9c72a8754ef
orliesaurus <orliesaurus@facebook.com>
2015-03-31 12:58:51 +0100

    first push

---
+++ README.md
@@ -0,0 +1,18 @@
+#Greetings Slackers
+This simple script written in bad Python will allow you to hook into the real time API of Slack and perform a greeting to every new joiner!
+Usually this is used to tell new joiners about the network, the guidelines, rules, useful links etc etc
+
+#Requirements
+Python 2.7+
+Edit `bot.py` on lines 7-8 to customise with your greeting and token
+
+#Installation
+```bash
+git clone <thisgitrepo>
+cd <thisgitrepo>
+virtualenv greetingslack
+. greetingslack/bin/activate
+pip install requests
+pip install websocket-client
+python bot.py &
+```
---
+++ bot.py
@@ -0,0 +1,47 @@
+import websocket
+import json
+import requests
+import urllib
+
+###VARIABLES THAT YOU NEED TO SET##############################
+MESSAGE = "Hello and Welcome to our pretty Slack Network! :)"
+TOKEN = "YOUR-TOKEN"
+###############################################################
+
+def parse_join(message):
+    m = json.loads(message)
+    if (m['type'] == "team_join"):
+        x = requests.get("https://slack.com/api/im.open?token="+TOKEN+"&user="+m["user"]["id"])
+        x = x.json()
+        x = x["channel"]["id"]
+        xx = requests.post("https://slack.com/api/chat.postMessage?token="+TOKEN+"&channel="+x+"&text="+urllib.quote(MESSAGE)+"&as_user=true")
+
+        #DEBUG
+        #print '\033[91m' + "HELLO SENT" + m["user"]["id"] + '\033[0m'
+        #
+
+#Connects to Slacks and initiates socket handshake        
+def start_rtm():
+    r = requests.get("https://slack.com/api/rtm.start?token="+TOKEN, verify=False)
+    r = r.json()
+    r = r["url"]
+    return r
+
+def on_message(ws, message):
+    parse_join(message)
+
+def on_error(ws, error):
+    print "SOME ERROR HAS HAPPENED", error
+
+def on_close(ws):
+    print '\033[91m'+"Connection Closed"+'\033[0m'
+
+def on_open(ws):
+    print "Connection Started - Auto Greeting new joiners to the network"
+
+
+if __name__ == "__main__":
+    r = start_rtm()
+    ws = websocket.WebSocketApp(r, on_message = on_message, on_error = on_error, on_close = on_close)
+    #ws.on_open
+    ws.run_forever()
