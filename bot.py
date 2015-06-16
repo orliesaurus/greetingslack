597e1313c6c8ded6be6f9b5d6a0ea3bf9b1907e8
orliesaurus <orliesaurus@users.noreply.github.com>
2015-06-16 11:28:34 +0100

    oops

--- bot.py
+++ bot.py
@@ -5,8 +5,8 @@ import urllib
 import os
 ###VARIABLES THAT YOU NEED TO SET MANUALLY IF NOT ON HEROKU#####
 try:
-	MESSAGE = os.environ['SLACK-TOKEN'] 
-	TOKEN = os.environ['WELCOME-MESSAGE']
+	MESSAGE = os.environ['WELCOME-MESSAGE'] 
+	TOKEN = os.environ['SLACK-TOKEN']
 except:
 	MESSAGE = 'Manually set the Message if youre not running through heroku or have not set vars in ENV'
 	TOKEN = 'Manually set the API Token if youre not running through heroku or have not set vars in ENV'
