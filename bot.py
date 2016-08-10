4ef638fb43aa59e8a36f1f6561e9ff95149053fd
Jer'Maine Montiel, Jr <jermaine.m.jones@outlook.com>
2016-08-10 13:33:18 -0500

    added parse parameter
    with "parse=full" being passed, the user can post any messages using #, @ or links without having to format them in the message. When "parse=full" is received Slack will search the message and add the proper formatting.

--- bot.py
+++ bot.py
@@ -18,7 +18,7 @@ def parse_join(message):
         x = requests.get("https://slack.com/api/im.open?token="+TOKEN+"&user="+m["user"]["id"])
         x = x.json()
         x = x["channel"]["id"]
-        xx = requests.post("https://slack.com/api/chat.postMessage?token="+TOKEN+"&channel="+x+"&text="+urllib.quote(MESSAGE)+"&as_user=true")
+        xx = requests.post("https://slack.com/api/chat.postMessage?token="+TOKEN+"&channel="+x+"&text="+urllib.quote(MESSAGE)+"&parse=full&as_user=true")
 
         #DEBUG
         #print '\033[91m' + "HELLO SENT" + m["user"]["id"] + '\033[0m'
